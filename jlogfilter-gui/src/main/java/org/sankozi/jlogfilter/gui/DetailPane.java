b6ecd4d9d3ccd493a7f830677611f80a0a148335
sankozi <sankozii@gmail.com>
2014-03-10 18:55:25 +0100

    improved detail pane

--- jlogfilter-gui/src/main/java/org/sankozi/jlogfilter/gui/DetailPane.java
+++ jlogfilter-gui/src/main/java/org/sankozi/jlogfilter/gui/DetailPane.java
@@ -40,37 +40,44 @@ public class DetailPane extends BorderPane {
             contentBuilder.append("<!DOCTYPE html>\n"
                                  + "<html lang=\"en\">\n"
                                  + "<style type=\"text/css\">"
-                                 + "pre { margin:0px; white-space:pre-wrap; }"
-                                 + "pre.emph {font-weight:bold; border:1px solid red; background-color:#FFFFCC;}"
+                                 + "p { margin-top:4px; margin-bottom:2px; font-weight:bold; }\n"
+                                 + "pre { margin:0px; white-space:pre-wrap; }\n"
+                                 + "pre.emph {font-weight:bold; border:1px solid red; background-color:#FFFFCC;}\n"
                                  + "</style>"
                                  + "<head>"
                                  + "</head><body>")
+                    .append("<p>Category:</p>")
+                    .append("<pre>").append(logEntry.getCategory()).append("</pre>")
+                    .append("<p>Message:</p>")
                     .append("<pre width='100%'>")
                     .append(logEntry.getMessage())
-                    .append('\n') ;
-            boolean emph = false;
-            for(String stack: logEntry.getStacktrace()){
-                boolean nextEmph;
-                if(stack.startsWith("\tat ")){
-                    String prefix = categoryPrefixes.floor(stack.substring(4));
-                    nextEmph = prefix != null && stack.startsWith("\tat " + prefix);
-                } else {
-                    String prefix = categoryPrefixes.floor(stack);
-                    nextEmph = prefix != null && stack.startsWith(prefix);
-                }
-                if(nextEmph){
-                    if(!emph){
-                        contentBuilder.append("</pre><pre class='emph'>");
-                        emph = true;
+                    .append("</pre>") ;
+            if(logEntry.getStacktrace().length > 0){
+                contentBuilder.append("<p>Stacktrace:</p>").append("<pre>");
+                boolean emph = false;
+                for(String stack: logEntry.getStacktrace()){
+                    boolean nextEmph;
+                    if(stack.startsWith("\tat ")){
+                        String prefix = categoryPrefixes.floor(stack.substring(4));
+                        nextEmph = prefix != null && stack.startsWith("\tat " + prefix);
+                    } else {
+                        String prefix = categoryPrefixes.floor(stack);
+                        nextEmph = prefix != null && stack.startsWith(prefix);
                     }
-                } else if (emph) {
-                    contentBuilder.append("</pre><pre>");
-                    emph = false;
+                    if(nextEmph){
+                        if(!emph){
+                            contentBuilder.append("</pre><pre class='emph'>");
+                            emph = true;
+                        }
+                    } else if (emph) {
+                        contentBuilder.append("</pre><pre>");
+                        emph = false;
+                    }
+                    contentBuilder.append(stack).append('\n');
                 }
-                contentBuilder.append(stack).append('\n');
+                contentBuilder.append("</pre>");
             }
-            contentBuilder.append("</pre>")
-                          .append("</body></html>");
+            contentBuilder.append("</body></html>");
             detailArea.getEngine().loadContent(contentBuilder.toString());
         } else {
             detailArea.getEngine().loadContent("<html></html>");
